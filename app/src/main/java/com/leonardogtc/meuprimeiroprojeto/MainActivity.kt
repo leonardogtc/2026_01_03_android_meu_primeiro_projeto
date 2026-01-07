47c6cd74f8ebc1098fc7a61876ec43897e81c3e0
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2026-01-07 09:00:11 -0300

    Adicionando evendo de clique ao botão

--- app/src/main/java/com/leonardogtc/meuprimeiroprojeto/MainActivity.kt
+++ app/src/main/java/com/leonardogtc/meuprimeiroprojeto/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.leonardogtc.meuprimeiroprojeto
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,4 +18,9 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+
+    fun clique(view: View) {
+        println("Was clicked")
+    }
+
 }
